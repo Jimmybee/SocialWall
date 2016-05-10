7476528aabca6e36b7efdcdbff580bdfab058193
James Birtwell <jsbirtwell@gmail.com>
2016-05-10 17:44:44 +0100

    Initial Commit

---
+++ SocialWall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialWall
+//
+//  Created by James Birtwell on 10/05/2016.
+//  Copyright © 2016 James Birtwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
